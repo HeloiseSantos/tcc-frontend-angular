f96b8d961488c9f118e33c66136cd49e3013b8df
Heloíse Silva Santos <santoshs@taginfraestrutura.com.br>
2022-10-12 17:12:11 -0300

    feature: cria componente para editar produtos

---
+++ src/app/shared/components/product/product-update/product-update.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductUpdateComponent } from './product-update.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductUpdateComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ProductUpdateComponent
+  ]
+})
+export class ProductUpdateModule { }
