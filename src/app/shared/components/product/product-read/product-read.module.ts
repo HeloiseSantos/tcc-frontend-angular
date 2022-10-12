4113190e9401ba7dbd850922c3f799c27aad213d
Heloíse Silva Santos <santoshs@taginfraestrutura.com.br>
2022-10-12 17:10:21 -0300

    feature: cria componente de listagem de produtos

---
+++ src/app/shared/components/product/product-read/product-read.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductReadComponent } from './product-read.component';
+
+@NgModule({
+  declarations: [
+    ProductReadComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ProductReadComponent
+  ]
+})
+export class ProductReadModule { }
