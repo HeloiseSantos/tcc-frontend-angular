a086c3159d39e914a46bc9f073c6528d91acdc53
Heloíse Silva Santos <santoshs@taginfraestrutura.com.br>
2022-10-12 16:49:21 -0300

    feature: criação dos botões do componente de criar produtos

--- src/app/shared/components/product/product-create/product-create.module.ts
+++ src/app/shared/components/product/product-create/product-create.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 import { ProductCreateComponent } from './product-create.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -8,6 +10,8 @@ import { ProductCreateComponent } from './product-create.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
+    MatButtonModule
   ],
   exports: [
     ProductCreateComponent
