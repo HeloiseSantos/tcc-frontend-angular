f5a48a105f34c4170193b66affe90e43a740f9ae
Heloíse Silva Santos <santoshs@taginfraestrutura.com.br>
2022-10-12 17:13:29 -0300

    feature: cria componente para deletar produtos

---
+++ src/app/shared/components/product/product-delete/product-delete.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductDeleteComponent } from './product-delete.component';
+
+@NgModule({
+  declarations: [
+    ProductDeleteComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ProductDeleteComponent
+  ]
+})
+export class ProductDeleteModule { }
