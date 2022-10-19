bf612d9ac8be271839fc7318a63cdd8dbd1452bb
Heloíse Silva Santos <santoshs@taginfraestrutura.com.br>
2022-10-18 23:33:35 -0300

    refactor: lista todos os itens cadastrados através de requisição via api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +19,7 @@ import { ProductCrudModule } from './modules/product-crud/product-crud.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HeaderModule,
@@ -25,6 +28,7 @@ import { ProductCrudModule } from './modules/product-crud/product-crud.module';
     MenuModule,
     HomeModule,
     ProductCrudModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
