41713452a92939326b29022c0512cadc40617d08
Heloise Santos <heloise.hssantos@gmail.com>
2022-11-09 23:24:42 -0300

    Altera o email exibido no menu

--- src/app/mock-api/common/user/data.ts
+++ src/app/mock-api/common/user/data.ts
@@ -2,7 +2,7 @@
 export const user = {
     id    : 'cfaad35d-07a3-4447-a6c3-d8c3d54fd5df',
     name  : 'Heloíse Santos',
-    email : 'heloise.hssantos@gmail.com',
+    email : 'heloise.santos@mabruhe.com',
     avatar: 'assets/images/avatars/heloise-santos.jpg',
     status: 'online'
 };
