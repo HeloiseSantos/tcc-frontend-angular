f9bbffec39c482f31d51e1e487c1601141408c2e
Heloise Santos <heloise.hssantos@gmail.com>
2022-11-26 15:59:02 -0300

    cria na service os métodos getById, update e delete

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -22,9 +22,39 @@ export class ProductService {
     });
   }
 
+  getById(id: number): Promise<HttpResponse<ProductModel>> {
+    return new Promise((resolve, reject) => {
+      this._httpClient.get(`${environment.api}/wines/${id}`, { observe: 'response' })
+        .subscribe({
+          next: (response: HttpResponse<ProductModel>) => resolve(response),
+          error: (error: HttpErrorResponse) => reject(error)
+        })
+    });
+  }
+
   create(product: ProductModel): Promise<HttpResponse<any>> {
     return new Promise((resolve, reject) => {
       this._httpClient.post(`${environment.api}/wines`, product, { observe: 'response' })
+        .subscribe({
+          next: (response: HttpResponse<any>) => resolve(response),
+          error: (error: HttpErrorResponse) => reject(error)
+        });
+    });
+  }
+
+  update(product: ProductModel): Promise<HttpResponse<any>> {
+    return new Promise((resolve, reject) => {
+      this._httpClient.put(`${environment.api}/wines/${product.id}`, product, { observe: 'response' })
+        .subscribe({
+          next: (response: HttpResponse<any>) => resolve(response),
+          error: (error: HttpErrorResponse) => reject(error)
+        });
+    });
+  }
+
+  delete(product: ProductModel): Promise<HttpResponse<any>> {
+    return new Promise((resolve, reject) => {
+      this._httpClient.delete(`${environment.api}/wines/${product.id}`, { observe: 'response' })
         .subscribe({
           next: (response: HttpResponse<any>) => resolve(response),
           error: (error: HttpErrorResponse) => reject(error)
