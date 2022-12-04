2e99ac422cc20d7d396241afc67a3cab52e47407
Heloise Santos <heloise.hssantos@gmail.com>
2022-12-04 00:45:42 -0300

    Organiza importações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ExtraOptions, PreloadAllModules, RouterModule } from '@angular/router';
-import { MarkdownModule } from 'ngx-markdown';
+import { registerLocaleData } from '@angular/common';
+import localePT from '@angular/common/locales/pt';
+
 import { FuseModule } from '@fuse';
 import { FuseConfigModule } from '@fuse/services/config';
 import { FuseMockApiModule } from '@fuse/lib/mock-api';
+
+import { MarkdownModule } from 'ngx-markdown';
 import { CoreModule } from 'app/core/core.module';
 import { appConfig } from 'app/core/config/app.config';
 import { mockApiServices } from 'app/mock-api';
@@ -13,8 +17,6 @@ import { LayoutModule } from 'app/layout/layout.module';
 import { AppComponent } from 'app/app.component';
 import { appRoutes } from 'app/app.routing';
 import { CurrencyMaskConfig, CurrencyMaskModule, CURRENCY_MASK_CONFIG } from 'ng2-currency-mask';
-import localePT from '@angular/common/locales/pt';
-import { registerLocaleData } from '@angular/common';
 
 registerLocaleData(localePT);
 
--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -1,17 +1,13 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule, registerLocaleData } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import localePt from '@angular/common/locales/pt';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
-import { ProductRoutingModule } from './product-routing.module';
-import { ProductReadComponent } from './product-read/product-read.component';
-import { ProductCreateComponent } from './product-create/product-create.component';
-import { ProductUpdateComponent } from './product-update/product-update.component';
-import { ProductDeleteComponent } from './product-delete/product-delete.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -19,11 +15,17 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSelectModule } from '@angular/material/select';
-import { ToastSharedService } from 'app/shared/services/toast-shared.service';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+
 import { CurrencyMaskModule } from 'ng2-currency-mask';
-import localePt from '@angular/common/locales/pt';
+import { ToastSharedService } from 'app/shared/services/toast-shared.service';
+
+import { ProductRoutingModule } from './product-routing.module';
+import { ProductReadComponent } from './product-read/product-read.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductUpdateComponent } from './product-update/product-update.component';
+import { ProductDeleteComponent } from './product-delete/product-delete.component';
 
 registerLocaleData(localePt)
 
