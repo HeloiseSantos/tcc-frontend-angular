6b0840b246de0b9b6ba37d398c5a20d17cfb0ecb
Heloise Santos <heloise.hssantos@gmail.com>
2022-12-09 21:43:36 -0300

    Ajustes menu e header

--- src/app/layout/layouts/vertical/classy/classy.module.ts
+++ src/app/layout/layouts/vertical/classy/classy.module.ts
@@ -7,12 +7,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { FuseNavigationModule } from '@fuse/components/navigation';
 import { FuseLoadingBarModule } from '@fuse/components/loading-bar';
-import { FuseFullscreenModule } from '@fuse/components/fullscreen/fullscreen.module';
 import { LanguagesModule } from 'app/layout/common/languages/languages.module';
 import { MessagesModule } from 'app/layout/common/messages/messages.module';
 import { NotificationsModule } from 'app/layout/common/notifications/notifications.module';
 import { QuickChatModule } from 'app/layout/common/quick-chat/quick-chat.module';
-import { SearchModule } from 'app/layout/common/search/search.module';
 import { ShortcutsModule } from 'app/layout/common/shortcuts/shortcuts.module';
 import { UserModule } from 'app/layout/common/user/user.module';
 import { SharedModule } from 'app/shared/shared.module';
@@ -31,14 +29,12 @@ import { MatTooltipModule } from '@angular/material/tooltip';
         MatIconModule,
         MatMenuModule,
         MatTooltipModule,
-        FuseFullscreenModule,
         FuseLoadingBarModule,
         FuseNavigationModule,
         LanguagesModule,
         MessagesModule,
         NotificationsModule,
         QuickChatModule,
-        SearchModule,
         ShortcutsModule,
         UserModule,
         SharedModule
